88d9b95be4f064b315510662e6d106b845a0a8b6
Caitlin Donahue <caitlindonahue95@gmail.com>
2019-10-02 22:47:09 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app >
+      <v-toolbar-title>Boundless Crafting Tracker</v-toolbar-title>
     </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <HelloWorld :recipes="Object.values(json)"/>
+    
+    <v-footer  app >
+      <span class="px-4">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
+import json from './json/crafting-recipes.json';
 
 export default {
   name: 'App',
@@ -31,7 +21,7 @@ export default {
     HelloWorld,
   },
   data: () => ({
-    //
+    json: json
   }),
 };
 </script>
